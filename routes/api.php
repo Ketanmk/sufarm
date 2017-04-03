a30bddc815b2b5fb1caba4a26dc68a9f299cd70c
cargie <cargiejayectin@gmail.com>
2017-04-04 01:45:17 +0800

    remove child gallery

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,13 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 Route::get('/user', function (Request $request) {
     return '123';
 })->middleware('auth:api');
-Route::group(['prefix' => 'v1', 'middleware' =>  'auth.basic'], function () {
+Route::group(['prefix' => 'v1', 'middleware' => 'auth.basic'], function () {
     Route::get('/galleries/{id}/photos', 'ApiPhotosController@index');
     Route::resource('/galleries', 'ApiGalleryController');
-    Route::get('/galleries/{id}/galleries', 'ApiGalleryController@index');
+    // Route::get('/galleries/{id}/galleries', 'ApiGalleryController@index');
     Route::resource('/photos', 'ApiPhotosController');
 });
